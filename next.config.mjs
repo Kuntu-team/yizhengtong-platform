fb687d21178df11e8c59b094c2bf315f335e5d98
wang_bing <15031068571>
2025-07-08 15:12:40 +0800

    update:更新打包输出

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
     unoptimized: true,
   },
   devIndicators: false,
-  output: "standalone" // 添加 standalone 输出模式配置
+  output: "standalone", // 添加 standalone 输出模式配置
+  distDir: 'next', // 这里自定义为 next
 }
 
 export default nextConfig
